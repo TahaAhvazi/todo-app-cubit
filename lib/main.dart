c01e5ce66e3332ad6894ddce8dd61023160be922
Taha <tahax.dev@gmail.com>
2022-06-29 13:17:27 +0430

    Add multiblocProvider(Injecting cubits)

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:todo_app_cubit/cubits/cubits.dart';
+import 'package:todo_app_cubit/cubits/filtred_todo_list/filtred_todo_list_cubit.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,21 +13,47 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => TodoFilterCubit(),
+        ),
+        BlocProvider(
+          create: (context) => TodoSearchCubit(),
+        ),
+        BlocProvider(
+          create: (context) => TodoListCubit(),
+        ),
+        BlocProvider(
+          create: (context) => ActiveTodoCountCubit(
+            todoListCubit: BlocProvider.of<TodoListCubit>(context),
+          ),
+        ),
+        BlocProvider(
+          create: (context) => FiltredTodoListCubit(
+            todoFilterCubit: BlocProvider.of<TodoFilterCubit>(context),
+            todoSearchCubit: BlocProvider.of<TodoSearchCubit>(context),
+            todoListCubit: context.read<
+                TodoListCubit>(), // no diffrent, we can also use BlocProvider
+          ),
+        )
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          // This is the theme of your application.
+          //
+          // Try running your application with "flutter run". You'll see the
+          // application has a blue toolbar. Then, without quitting the app, try
+          // changing the primarySwatch below to Colors.green and then invoke
+          // "hot reload" (press "r" in the console where you ran "flutter run",
+          // or simply save your changes to "hot reload" in a Flutter IDE).
+          // Notice that the counter didn't reset back to zero; the application
+          // is not restarted.
+          primarySwatch: Colors.blue,
+        ),
+        home: const MyHomePage(title: 'Flutter Demo Home Page'),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
